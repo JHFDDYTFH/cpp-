0f540120f2f99d211bc8c273fcae159731b2e40b
JHFDDYTFH <3635086256@qq.com>
2025-11-29 16:34:07 +0800

    错误思路

---
+++ 实验3/exp409.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, a, b, c;
+    for(i=1,j=0;a*a*a+b*b*b+c*c*c==a*100+b*10+c;i++,j++)
+    i+=a;j+=b;j+=c;
+    cout<<a<<""<<b<<""<<c<<""<<endl;
+}
