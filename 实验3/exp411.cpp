f907747e348a015ad20253ea62a4fcdc9996f6be
JHFDDYTFH <3635086256@qq.com>
2025-11-29 17:34:58 +0800

    错误思路

---
+++ 实验3/exp411.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i,a,b,c,d;
+   for(int i=0;i<4;i++)
+   cout<<"Please enter a number";cin>>a;
+   if(a>='0'&&a<='0')
+   {cout<<"Please enter a number";cin>>b;}
+   else 
+   break;
+   if(b>='0'&&b<='0')
+    cout<<"Please enter a number";cin>>c;
+   if(c>='0'&&c<='0')
+    cout<<"Please enter a number";cin>>d;
+
+
+}
