6534eb844c26ad0477e461735143a61b63b81d77
JHFDDYTFH <3635086256@qq.com>
2025-11-29 17:02:43 +0800

    错误思路

---
+++ 实验3/exp410求不等式.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,value,s,sum;
+    
+    cout<<"please enter a value";cin>>value;
+    for(i=1;i>i;i++)
+    {s=1/i; s+=sum;}
+    if(sum>value)
+    cout<<i;
+
+
+
+}
