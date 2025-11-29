db47522c6e12c5dfadce4c42010e8b6a1f0c186e
JHFDDYTFH <3635086256@qq.com>
2025-11-29 17:09:06 +0800

    修改后用for

--- 实验3/exp410求不等式.cpp
+++ 实验3/exp410求不等式.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 int main()
 {
-    int i,value,s,sum;
-    
-    cout<<"please enter a value";cin>>value;
-    for(i=1;i>i;i++)
-    {s=1/i; s+=sum;}
-    if(sum>value)
-    cout<<i;
-
-
-
+    int value;
+    cout<<"please enter a value ";cin>>value;
+    double sum=0.0;
+    int n;
+    for(n=1;;n++)
+    { sum+=1.0/n;
+    if(sum>value){
+        break;
+    }
+}
+    cout<<"The minimun n is "<<n;
+    return 0;
 }
