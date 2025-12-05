6320459b56f6c6f7a0c54536758fcce7da848339
JHFDDYTFH <3635086256@qq.com>
2025-12-05 08:21:33 +0800

    第二小问

---
+++ 实验4/exp501(2).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j;
+    for(i=1;i<=4;i++)
+    {for(j=1;j<=i;j++)
+        printf("%4d%4d",i,j);
+        printf("\n");
+    }
+    return 0;
+}
