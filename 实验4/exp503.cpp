6a74767a7c58cc86e95b7e87f9576d872f6bd43b
JHFDDYTFH <3635086256@qq.com>
2025-12-06 15:19:11 +0800

    正确答案

--- 实验4/exp503.cpp
+++ 实验4/exp503.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main(){
     int x,y,z;
     for(x=1;x<=20;x++)
-    for(y=1;y<=33;y++){
+    for(y=1;y<=(100-5*x)/3;y++){
         z=100-x-y;
-        if(x*5+y*3+z/3==100)
+        if(x*15+y*9+z==300)
         
             cout<<x<<" "<<y<<" "<<z<<endl;
         
