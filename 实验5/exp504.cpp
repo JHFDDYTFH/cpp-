bc289929382e92c1f14044cae7be05bb49174cf3
JHFDDYTFH <3635086256@qq.com>
2025-12-16 18:31:52 +0800

    排序不会写

--- 实验5/exp504.cpp
+++ 实验5/exp504.cpp
@@ -8,14 +8,28 @@
    for(i=0; i<10; i++) //输入10个数
      cin>>a[i];
    cout<<endl;
-           ;//函数调用,数组名作实参
+    select_sort(a,10)      ;//函数调用,数组名作实参
    cout<<"the sorted array:"<<endl;
    for(i=0; i<10; i++) 
-            ; //输出10个已排好序的数
+   cout<<a[i]<<" "      ; //输出10个已排好序的数
    cout<<endl;
    return 0;
  }
- void select_sort(int array[],int n)//形参array是数组名
+ void select_sort(int array[],int n)//形参array是数组名//不会啊啊啊啊啊啊啊啊啊
  {
-               //排序
+  int i, j, min_idx, temp;
+    // 外层循环：遍历待排序区间的起始位置
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i; // 假设当前起始位置是最小值的下标
+        // 内层循环：找剩余区间的最小值下标
+        for (j = i + 1; j < n; j++) {
+            if (array[j] < array[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // 交换当前起始位置和最小值的位置
+        temp = array[i];
+        array[i] = array[min_idx];
+        array[min_idx] = temp;
+    }           //排序
  }
