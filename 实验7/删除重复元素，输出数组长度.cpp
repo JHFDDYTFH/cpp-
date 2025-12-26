94d598e61ae1669d083979566193e290a544badb
JHFDDYTFH <3635086256@qq.com>
2025-12-26 08:59:30 +0800

    把a中重复的元素存到b中

---
+++ 实验7/删除重复元素，输出数组长度.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[]={0,0,1,1,1,2,2,3,3,4};
+    int n=sizeof(a)/sizeof(a[0]);
+    int b[10]={0};
+    int count=0;
+    bool isExist=false;
+    for(int i=0;i<n;i++)
+    {
+    
+        isExist=false;
+        
+            for(int j=0;j<count;j++)
+            {
+            if(a[i]==b[j])
+           {
+            isExist=true;
+            break;
+           }
+    
+    }
+    if(!isExist)
+    {
+        b[count]=a[i];
+        count++;
+    }
+    }for(int k=0;k<count;k++)
+    {
+       cout<<b[k] ;
+    }
+return 0;
+}
